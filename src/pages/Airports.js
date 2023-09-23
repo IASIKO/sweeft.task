5a8252095ffa772f194d6f7ee53e490cb27dc760
IASIKO <iaseshviligi@gmail.com>
2023-09-23 21:42:47 +0400

    update airports data

--- src/pages/Airports.js
+++ src/pages/Airports.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import StyledBoxComponent from "../UI/StyledBoxComponent";
 import { FormControl, Grid, Input, Typography } from "@mui/material";
+import { Context } from "../store/ContextProvider";
 
 const Airports = () => {
+  const { selectedCountry } = useContext(Context);
   const [airportsData, setAirportsData] = useState([]);
 
   const fetchAirports = async (country) => {
+    console.log(country);
     const response = await fetch(
-      `https://api.api-ninjas.com/v1/airports?name=${country}'`,
+      `https://api.api-ninjas.com/v1/airports?country=${country}&name=`,
       {
         headers: { "X-Api-Key": "gCj5I7rGBqfKSYWRfkr2+g==QgM7Oha3XwapUYSa" },
       }
@@ -17,9 +20,13 @@ const Airports = () => {
     setAirportsData(data);
   };
 
+  console.log(selectedCountry?.cca2);
+
   useEffect(() => {
-    fetchAirports();
-  }, []);
+    if (selectedCountry?.cca2) {
+      fetchAirports(selectedCountry?.cca2);
+    }
+  }, [selectedCountry]);
 
   console.log(airportsData);
 
@@ -31,12 +38,18 @@ const Airports = () => {
       <FormControl variant="standard">
         <Input placeholder="Search for airport" />
       </FormControl>
-      <Grid container spacing={{ xs: 2 }}>
-        <Grid item xs={12} md={6} mt={2}>
-          <Typography variant="body1">
-            KUT - Kopitnari Airport (Kutaisi)
-          </Typography>
-        </Grid>
+      <Grid container spacing={{ xs: 2 }} style={{ marginTop: "16px" }}>
+        {airportsData.map((a, index) => (
+          <React.Fragment key={index}>
+            {a.iata && (
+              <Grid item xs={12} md={6}>
+                <Typography variant="body1">
+                  {a.iata} - {a.name} ({a.city})
+                </Typography>
+              </Grid>
+            )}
+          </React.Fragment>
+        ))}
       </Grid>
     </StyledBoxComponent>
   );
