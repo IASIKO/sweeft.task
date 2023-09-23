c6d44dfe64c76fa744a0edf1bc662eb135004d42
IASIKO <iaseshviligi@gmail.com>
2023-09-23 22:55:28 +0400

    update search logic on airports page

--- src/pages/Airports.js
+++ src/pages/Airports.js
@@ -6,10 +6,11 @@ import { Context } from "../store/ContextProvider";
 const Airports = () => {
   const { selectedCountry } = useContext(Context);
   const [airportsData, setAirportsData] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-  const fetchAirports = async (country) => {
+  const fetchAirports = async (country, name) => {
     const response = await fetch(
-      `https://api.api-ninjas.com/v1/airports?country=${country}&name=`,
+      `https://api.api-ninjas.com/v1/airports?country=${country}&name=${name}`,
       {
         headers: { "X-Api-Key": "gCj5I7rGBqfKSYWRfkr2+g==QgM7Oha3XwapUYSa" },
       }
@@ -20,10 +21,23 @@ const Airports = () => {
   };
 
   useEffect(() => {
-    if (selectedCountry?.cca2) {
-      fetchAirports(selectedCountry?.cca2);
-    }
-  }, [selectedCountry]);
+    const timerId = setTimeout(() => {
+      if (searchValue && selectedCountry?.cca2) {
+        fetchAirports(selectedCountry.cca2, searchValue);
+      } else {
+        if (selectedCountry?.cca2) {
+          fetchAirports(selectedCountry?.cca2);
+        }
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [selectedCountry, searchValue]);
+
+  const filteredAirports = airportsData.filter((airport) =>
+    airport.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   return (
     <StyledBoxComponent>
@@ -31,11 +45,15 @@ const Airports = () => {
         Airports{" "}
       </Typography>
       <FormControl variant="standard">
-        <Input placeholder="Search for airport" />
+        <Input
+          placeholder="Search for airport"
+          value={searchValue}
+          onChange={(event) => setSearchValue(event.target.value)}
+        />
       </FormControl>
       <Grid container spacing={{ xs: 2 }} style={{ marginTop: "16px" }}>
-        {airportsData.length > 0 ? (
-          airportsData.map((a, index) => (
+        {filteredAirports.length > 0 ? (
+          filteredAirports.map((a, index) => (
             <React.Fragment key={index}>
               {a.iata && (
                 <Grid item xs={12} md={6}>
