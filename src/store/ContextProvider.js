853eb9604f1f8b66400e53d59a36ca8da19a2e00
IASIKO <iaseshviligi@gmail.com>
2023-09-28 00:13:34 +0400

    save

--- src/store/ContextProvider.js
+++ src/store/ContextProvider.js
@@ -39,7 +39,7 @@ const ContextProvider = ({ children }) => {
       navigator.geolocation.getCurrentPosition(succesFunction, errorFunction);
     } else {
       setCountryCode(countriesData.find((c) => c.cca3 === currentRoute)?.cca2);
-        setSelectedCountry(countriesData.find((c) => c.cca3 === currentRoute));
+      setSelectedCountry(countriesData.find((c) => c.cca3 === currentRoute));
     }
   };
 
