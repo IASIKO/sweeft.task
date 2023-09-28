2ad4bf0fdf7372a203029f05f49ddc3d15bfc4ba
IASIKO <iaseshviligi@gmail.com>
2023-09-28 14:58:14 +0400

    update basename

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import "./App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  HashRouter,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import Airports from "./pages/Airports";
 import Main from "./pages/Main";
 import Exchange from "./pages/Exchange";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-  },
-  {
-    path: "/:country",
-    element: <Main />,
-    children: [
-      { path: "/:country", element: <Exchange /> },
-      { path: "/:country/airports", element: <Airports /> },
-    ],
-  },
-], {
-  basename: '/sweeft.task/'
-});
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+    },
+    {
+      path: "/:country",
+      element: <Main />,
+      children: [
+        { path: "/:country", element: <Exchange /> },
+        { path: "/:country/airports", element: <Airports /> },
+      ],
+    },
+  ],
+  {}
+);
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <HashRouter basename="/sweeft.task">
+      <RouterProvider router={router}></RouterProvider>;
+    </HashRouter>
+  );
 }
 
 export default App;
